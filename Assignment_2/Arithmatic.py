696c62bac72717c09c86026e87023e96469befd1
Prajwal Dixit <dixitpp04@gmail.com>
2026-06-12 17:18:29 +0530

    Add files via upload

---
+++ Assignment_2/Arithmatic.py
@@ -0,0 +1,8 @@
+no1 = float(input("Enter first number: "))
+no2 = float(input("Enter second number: "))
+
+print(no1 + no2)
+print(no1 - no2)
+print(no1 * no2)
+print(no1 / no2)
+
---
+++ Assignment_2/TypeId.py
@@ -0,0 +1,5 @@
+x = 20
+
+print(x)
+print(type(x))
+print(id(x))
